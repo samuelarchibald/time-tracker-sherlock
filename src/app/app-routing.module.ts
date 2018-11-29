b52705d3ddb59d843cbf8574ad923af7eddf3de3
samamaniac <sam.clark@me.com>
2018-11-29 14:43:57 -0800

    update1
    Added basic component and styled header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientComponent } from './client/client.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: '', component: DashboardComponent, data: {state: 'dashboard'}},
+	{path: 'projects', component: ProjectListComponent, data: {state: 'project-list'}},
+	{path: 'clients', component: ClientComponent, data: {state: 'clients'}},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { ProjectComponent } from './project/project.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ClientListComponent } from './client-list/client-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TrackerComponent } from './tracker/tracker.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    ProjectComponent,
+    ProjectListComponent,
+    ClientListComponent,
+    DashboardComponent,
+    TrackerComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
